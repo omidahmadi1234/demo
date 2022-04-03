d4642a889048ef5a9fb1088fcba040ca63ce897f
omid ahmadi <omid.se12@gmail.com>
2022-04-03 23:05:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import  {users} from './users';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'teamG';
+
+  fomr_is_valid = true; 
+
+
+  nom = '';
+  prenom = '';
+  gendre = '';
+  // date de naissance 
+  dob = new Date(); 
+
+
+  data: users[] = [];
+
+  register(event:any){
+
+    if( this.nom  == "" || this.prenom == "" || this.gendre == "" || this.dob == null){
+      this.fomr_is_valid = false; 
+      setTimeout(()=>{
+        this.fomr_is_valid = true; 
+      }, 3000);
+    }else {
+
+      // form est valide
+      let new_user = new users(this.nom, this.prenom, this.gendre,this.dob);
+
+      this.data.push(new_user);
+      
+      this.nom = "";
+      this.prenom = "";
+      this.gendre = "";
+      this.dob = new Date;
+    }
+  
+  }
+
+  remove(id:any){
+    delete this.data[id]; 
+
+    console.log(this.data);
+  }
+  
 }
---
+++ src/app/users.ts
@@ -0,0 +1,15 @@
+export class users {
+  
+    nom:String;
+    prenom:String;
+    gendre:String;
+    dob:Date;
+    constructor(nom:String, prenom:String, gendre:String, dob:Date){
+      this.nom = nom; 
+      this.prenom = prenom;
+      this.gendre = gendre;
+      this.dob = dob; 
+  
+    }
+
+  }
